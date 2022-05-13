55e8b39fc784e3840453246888d8b00474ef4033
Jysai <jysai59@gmail.com>
2022-05-13 22:34:42 +0200

    intégration de la page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,32 +1,35 @@
 function photographerFactory(data) {
-    const { name, city, price, tagline, portrait } = data;
+    const { name, city, price, country, tagline, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        const cityPhotographer = document.createElement( 'span' );
-        cityPhotographer.innerText = city;
-        const taglinePhotographer = document.createElement( 'span' );
-        taglinePhotographer.innerText = tagline;
-        const pricePhotographer = document.createElement( 'span' );
-        pricePhotographer.innerText = price;
- 
+        // const img = document.createElement( 'img' );
+        // img.setAttribute("src", picture)
+        // const h2 = document.createElement( 'h2' );
+        // h2.textContent = name;
+
         
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(cityPhotographer);
-        article.appendChild(taglinePhotographer);
-        article.appendChild(pricePhotographer);
+        article.innerHTML = `
+        <div class="focus-photographer" aria-label="accéder au profil du photographe">
+        <img src=${picture} class="picture-photographer" alt="photo de profile du photographe">
+        <h2 class="">${name}</h2>
+        </div>
+        <div class="informations-photographer">
+        <span class="city-photographer">${city}, ${country} </span>
+        <span class="tagline-photographer">${tagline}</span>
+        <span class="price-photographer">${price}€/jour</span>
+        </div>`
+
+        // article.appendChild(cityPhotographer);
+        // article.appendChild(taglinePhotographer);
+        // article.appendChild(infosPhotographer);
+        
         
 
-        img.classList.add("picture-photographer")
 
         return (article);
     }
-    return { name, city, picture, price, tagline, getUserCardDOM }
+    return { name, city, picture, price, country,tagline, getUserCardDOM }
 }
